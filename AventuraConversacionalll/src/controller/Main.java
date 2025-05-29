2f4aa61ae6dad940b56a789799ded66c9e09286d
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-05-29 13:26:05 +0200

    WIP GameManager
    Prueba Combate GameManager

--- AventuraConversacionalll/src/controller/Main.java
+++ AventuraConversacionalll/src/controller/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import dao.DaoEnemigo;
 import dao.DaoProtagonista;
+import model.GameManager;
 import model.escenario.Escenario;
 import model.personajes.*;
 
@@ -12,27 +13,31 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 
-		Protagonista p1 = DaoProtagonista.getInstance().generarProtagonista(1);
-
-		p1.imprimirInfo();
-
-		Enemigo e1 = DaoEnemigo.getInstance().generarEnemigo(1);
-
-		e1.imprimirInfo();
-
-		Combate comb = new Combate();
+//		Protagonista p1 = DaoProtagonista.getInstance().generarProtagonista(1);
+//
+//		p1.imprimirInfo();
+//
+//		Enemigo e1 = DaoEnemigo.getInstance().generarEnemigo(1);
+//
+//		e1.imprimirInfo();
+//
+//		Combate comb = new Combate();
+//		
+//		ArrayList<Enemigo> misEnemigos = new ArrayList<Enemigo>();
+//		
+//		misEnemigos = DaoEnemigo.getInstance().generarEnemigos(1);
+//		
+//		for (Enemigo e : misEnemigos) {
+//			System.out.println(e.getNombre());
+//		}
+//		// Combate con el enemigo 5
+//		comb.inicioCombate(p1, misEnemigos.get(4));
+//		// Combate con el enemigo 1
+//		comb.inicioCombate(p1, misEnemigos.get(0));
 		
-		ArrayList<Enemigo> misEnemigos = new ArrayList<Enemigo>();
+		GameManager gm = new GameManager();
 		
-		misEnemigos = DaoEnemigo.getInstance().generarEnemigos(1);
-		
-		for (Enemigo e : misEnemigos) {
-			System.out.println(e.getNombre());
-		}
-		// Combate con el enemigo 5
-		comb.inicioCombate(p1, misEnemigos.get(4));
-		// Combate con el enemigo 1
-		comb.inicioCombate(p1, misEnemigos.get(0));
+		gm.start();
 		
 	}
 }
--- AventuraConversacionalll/src/model/GameManager.java
+++ AventuraConversacionalll/src/model/GameManager.java
@@ -1,12 +1,25 @@
 package model;
 
+import java.sql.SQLException;
+
+import dao.DaoEnemigo;
+import dao.DaoProtagonista;
+import model.personajes.*;
+
 public class GameManager {
+	
+	private Protagonista p1;
+	private Enemigo enemy;
+	private Combate comb;
 
 	public GameManager() {
 		
 	}
 	
-	public void start() {
-		
+	public void start() throws SQLException {
+		p1 = DaoProtagonista.getInstance().generarProtagonista(1);
+		enemy = DaoEnemigo.getInstance().generarEnemigo(1);
+		comb = new Combate();
+		comb.inicioCombate(p1, enemy);
 	}
 }
